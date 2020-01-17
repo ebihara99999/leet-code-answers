b6d03433cafad1aaae0ccb0f8018dedcc2321c15
Yusuke Ebihara <dev.biibiebi@gmail.com>
2020-01-18 01:10:13 +0900

    Accepted :tada:

--- string_to_integer/string_to_integer.rb
+++ string_to_integer/string_to_integer.rb
@@ -4,4 +4,11 @@ def my_atoi(str)
   valid_characters = ['-', '+', '1', '2', '3', '4', '5', '6', '7', '8' ,'9', '0']
   str.lstrip!
   return 0 unless valid_characters.include? str[0]
+  number = str.lstrip.split(' ').first.to_i
+
+  max = 2 ** 31 - 1
+  min = (-2) ** 31
+
+  return number if number.between?(min, max)
+  number > 0 ? max : min
 end
